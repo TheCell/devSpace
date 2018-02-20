9c37e90349cff4da35aa7b6269564677cf2680e4
TheCell <simon_hischier@hotmail.com>
2018-02-20 20:07:41 +0100

    added p5 instancing

--- FloydSteinbergDithering/start.js
+++ FloydSteinbergDithering/start.js
@@ -1,14 +1,52 @@
-let img;
-
-function preload()
+let inputSketch = function (p)
 {
-	img = loadImage("images/snowboardLittle.jpg");
+	let img = null;
+
+	p.preload = function ()
+	{
+		img = p.loadImage("images/snowboardLittle.jpg");
+	}
+
+	p.setup = function ()
+	{
+		p.createCanvas(img.width / 2, img.height / 2);
+		p.image(img, 0, 0, img.width / 2, img.height / 2);
+	}
+
+p.draw = function ()
+	{
+
+	}
 }
 
-function setup()
+let outputSketch = function (p)
 {
-	createCanvas(img.width / 2, img.height / 2);
-	image(img, 0, 0, img.width / 2, img.height / 2);
-	createCanvas(img.width / 2, img.height / 2);
-	image(img, 0, 0, img.width / 2, img.height / 2);
-}
+	let img = null;
+
+	p.preload = function ()
+	{
+		img = p.loadImage("images/snowboardLittle.jpg");
+	}
+
+	p.setup = function ()
+	{
+		p.createCanvas(img.width / 2, img.height / 2);
+		p.image(img, 0, 0, img.width / 2, img.height / 2);
+	};
+
+	p.draw = function ()
+	{
+
+	};
+
+	// p.mousePressed = function ()
+	// {
+	// 	gray = (gray + 16) % 256;
+	// };
+};
+
+let inputImageDiv = document.getElementById('inputImageDiv');
+let outputImageDiv = document.getElementById('outputImageDiv');
+
+new p5(inputSketch, inputImageDiv);
+new p5(outputSketch, outputImageDiv);
