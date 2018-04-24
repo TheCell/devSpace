c46923eb731844d9dc10d58e91e01ec84e0af237
TheCell <simon_hischier@hotmail.com>
2018-04-24 16:22:31 +0200

    upscaled to two inputs

--- IDA_WearableFutures/Sketches/ejtech_Resistor1/ejtech_Resistor1.ino
+++ IDA_WearableFutures/Sketches/ejtech_Resistor1/ejtech_Resistor1.ino
@@ -1,4 +1,5 @@
 int sensorPin = A1;
+int sensorPin2 = A2;
 
 void setup() {
  Serial.begin(9600); // put your setup code here, to run once:
@@ -6,9 +7,13 @@ void setup() {
 }
 
 void loop() {
-  long a = analogRead(A1);
+  long a = analogRead(sensorPin);
+  long b = analogRead(sensorPin2);
   Serial.print("a");
   Serial.print(a);
+  Serial.print("b");
+  Serial.print(b);
+ 
   Serial.println("");
   delay (100);
   // put your main code here, to run repeatedly:
