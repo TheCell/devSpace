65df85459d160d792d01cf05c88d11f6686df1b9
TheCell <simon_hischier@hotmail.com>
2018-05-17 15:56:27 +0200

    added ballswitch

--- IDA_WearableFutures/Sketches/IR_LED/IR_LED.ino
+++ IDA_WearableFutures/Sketches/IR_LED/IR_LED.ino
@@ -1,17 +1,21 @@
 int potPin = 2;
 int LEDS = 3;
+int switchPin = 2;
 int val = 0;
 
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(LEDS, OUTPUT);
+  pinMode(switchPin, INPUT);
+  Serial.begin(9600);      // open the serial port at 9600 bps:    
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   val = analogRead(potPin);    // read the value from the sensor
+  Serial.println(digitalRead(switchPin));
   digitalWrite(LEDS, HIGH);  // turn the ledPin on
   delay(val);                  // stop the program for some time
   digitalWrite(LEDS, LOW);   // turn the ledPin off
