7b6ae89b974584861f183fd59936d6b43d0eed30
TheCell <simon_hischier@hotmail.com>
2018-05-18 09:18:17 +0200

    added piezo

--- IDA_WearableFutures/Sketches/IR_LED/IR_LED.ino
+++ IDA_WearableFutures/Sketches/IR_LED/IR_LED.ino
@@ -2,12 +2,14 @@ int potPin = 2;
 int LEDS = 3;
 int switchPin = 2;
 int val = 0;
+int buzzerPin = 4;
 
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(LEDS, OUTPUT);
   pinMode(switchPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
   Serial.begin(9600);      // open the serial port at 9600 bps:    
 }
 
@@ -17,6 +19,13 @@ void loop()
   val = analogRead(potPin);    // read the value from the sensor
   Serial.println(digitalRead(switchPin));
   digitalWrite(LEDS, HIGH);  // turn the ledPin on
+  
+  for (int i = 0; i < val; i++)
+  {
+      digitalWrite(buzzerPin, HIGH);  // turn the ledPin on
+      delay(1);
+      digitalWrite(buzzerPin, LOW);   // turn the ledPin off
+  }
   delay(val);                  // stop the program for some time
   digitalWrite(LEDS, LOW);   // turn the ledPin off
   delay(val);                  // stop the program for some time
