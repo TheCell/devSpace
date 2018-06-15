b77f3484939ad5b0439c9d98fe0582075ba716e7
TheCell <simon_hischier@hotmail.com>
2018-06-15 16:55:24 +0200

    sketch for 21 Gram project

---
+++ IDA_WearableFutures/Sketches/21Grams/21Grams.ino
@@ -0,0 +1,49 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int oldValue = 0;
+int errorTolerance = 50;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  // initiate with basic values
+  oldValue = 0;
+  for(int i = 0; i < 10; i++)
+  {
+    oldValue += analogRead(sensorPin);
+  }
+  oldValue = oldValue / 10;
+}
+
+void loop() {
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+  // read the value from the sensor:
+  sensorValue = 0;
+  for(int i = 0; i < 10; i++)
+  {
+    sensorValue += analogRead(sensorPin);
+  }
+  sensorValue = sensorValue / 10;
+
+  if (oldValue > (sensorValue + errorTolerance) || oldValue < (sensorValue - errorTolerance))
+  {
+      // turn the ledPin on
+      digitalWrite(ledPin, HIGH);
+      // stop the program for for <sensorValue> milliseconds:
+      Serial.println("Changed");
+  }
+
+  /*
+  Serial.print("seonsorValue ");
+  Serial.println(sensorValue);
+  Serial.print("oldValue ");
+  Serial.println(oldValue);
+  */
+  oldValue = sensorValue;
+  // stop the program for <sensorValue> milliseconds:
+  delay(30);
+}
