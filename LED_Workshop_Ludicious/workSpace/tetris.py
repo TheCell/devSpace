8bd5d31760c91765d52b03d31876adaa798cdbf5
TheCell <simon_hischier@hotmail.com>
2019-02-03 00:49:42 +0100

    4 colors and restart if you lose

--- LED_Workshop_Ludicious/workSpace/tetris.py
+++ LED_Workshop_Ludicious/workSpace/tetris.py
@@ -4,7 +4,6 @@ import time
 import random
 
 amountOfLeds = 30
-lost = False
 leds = neopixel.NeoPixel(machine.Pin(4), amountOfLeds)
 
 brightness = machine.ADC(machine.Pin(33))
@@ -16,108 +15,110 @@ class Block:
   def __init__(self, pos, color):
     self.pos = pos
     self.color = color
-  
+
   def draw(self, leds):
     leds[int(self.pos)] = self.color
-
 
 
 def map(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def chooseColor(colorInput, brightness):
-  if brightness == brightnessSetting[0]:
+def chooseColor(colorInput, brightness, colorSettingsArr, brightnessSettingsArr):
+  if brightness == brightnessSettingsArr[0]:
     brightVal = 1
-  if brightness == brightnessSetting[1]:
+  if brightness == brightnessSettingsArr[1]:
     brightVal = 20
-  if brightness == brightnessSetting[2]:
-    brightVal = 100
-  
-  #brightness = brightness * 85 + 1
-  
-  if colorInput == colorSetting[0]:
+  if brightness == brightnessSettingsArr[2]:
+    brightVal = 150
+
+  if colorInput == colorSettingsArr[0]:
     return (brightVal,0,0)
-  if colorInput == colorSetting[1]:
+  if colorInput == colorSettingsArr[1]:
     return (0, brightVal,0)
-  if colorInput == colorSetting[2]:
+  if colorInput == colorSettingsArr[2]:
     return (0, 0, brightVal)
+  if colorInput == colorSettingsArr[3]:
+    return (brightVal, brightVal, 0)
 
 def resetLeds(ledArr, blockArr):
   for x in range(len(blockArr), amountOfLeds):
     ledArr[x] = (0, 0, 0)
     ledArr.write()
- 
+
 def drawBlocks(ledArr, blockArr):
   for x in range(0, len(blockArr)):
+
       blockArr[x].draw(ledArr)
       ledArr.write()
 
 def checkAndRemove(blockArr):
   if len(blockArr) <= 1:
     return
-  
+
   hasRemoved = False
-  
   amountTheSameColor = 0
   for x in range(1, len(blockArr)):
     prevItem = blockArr[x - 1]
-    
+
     if blockArr[x].color == prevItem.color:
       amountTheSameColor = amountTheSameColor + 1
     else:
       amountTheSameColor = 0
-    
+
     if amountTheSameColor == 3:
       for i in range(blockArr[x].pos, blockArr[x].pos - 4, -1):
         blockArr.pop(i)
       hasRemoved = True
-  
   return hasRemoved
-    
-    #if amountTheSameColor == -1:
-    #  prevItem = blockArr[x]
-    #  amountTheSameColor = 0
 
-def setNewColorRange(arr):
-  firstVal = arr[0]
-  arr.pop(0)
-  arr.append(firstVal)
-  
-  
-def setNewBrightnessRange(arr):
-  firstVal = arr[0]
-  arr.pop(0)
-  arr.append(firstVal)
-  
-  
-blocks = []
-colorSetting = [0, 1, 2]
-brightnessSetting = [0, 1, 2]
-lastDroppedPosition = -1
-dropPosition = amountOfLeds
+def shuffleArray(arr):
+  newArr = []
+  while(len(arr) > 0):
+    randomVal = random.choice(arr)
+    newArr.append(randomVal)
+    arr.remove(randomVal)
+  return newArr
 
-while not lost:
-  resetLeds(leds, blocks)
-  drawBlocks(leds, blocks)
-  
-  brightVal = int(map(brightness.read(), 0, 4095, 0, 2))
-  colorVal = int(map(color.read(), 0, 4095, 0, 2))
-  dropPosition -= 1
-  if dropPosition == lastDroppedPosition + 1:
-    block = Block(dropPosition, chooseColor(colorVal, brightVal))
-    blocks.append(block)
-    lastDroppedPosition = dropPosition
-    dropPosition = amountOfLeds - 1
-    setNewColorRange(colorSetting)
-    setNewBrightnessRange(brightnessSetting)
-  
-  if (lastDroppedPosition == amountOfLeds - 1):
-    lost = True
-  leds[dropPosition] = chooseColor(colorVal, brightVal)
-  leds.write()
-  
-  if checkAndRemove(blocks):
-    lastDroppedPosition = lastDroppedPosition - 4
-  #print(int(map(analogPin.read(), 0, 4095, 0, 255)))
-  #print(analogPin.read())
-  time.sleep_ms(50)
+def playTetris():
+  lost = False
+  blocks = []
+  colorSetting = [0, 1, 2, 3]
+  brightnessSetting = [0, 1, 2]
+  lastDroppedPosition = -1
+  dropPosition = amountOfLeds
+  tetrisDelay = 150
+
+  while not lost:
+    brightVal = int(map(brightness.read(), 0, 4095, 0, len(brightnessSetting) - 1))
+    colorVal = int(map(color.read(), 0, 4095, 0, len(colorSetting) - 1))
+    dropPosition -= 1
+
+    if dropPosition == lastDroppedPosition + 1:
+      block = Block(dropPosition, chooseColor(colorVal, brightVal, colorSetting, brightnessSetting))
+      blocks.append(block)
+      lastDroppedPosition = dropPosition
+      dropPosition = amountOfLeds - 1
+      colorSetting = shuffleArray(colorSetting)
+      brightnessSetting = shuffleArray(brightnessSetting)
+
+
+    if (lastDroppedPosition == amountOfLeds - 2):
+      lost = True
+
+    # draw gameparts
+    resetLeds(leds, blocks)
+    drawBlocks(leds, blocks)
+    leds[dropPosition] = chooseColor(colorVal, brightVal, colorSetting, brightnessSetting)
+    leds.write()
+
+    if checkAndRemove(blocks):
+      lastDroppedPosition = lastDroppedPosition - 4
+      if tetrisDelay > 10:
+        tetrisDelay -= 10
+    time.sleep_ms(tetrisDelay)
+
+
+# call to action
+inGameLoop = True
+while inGameLoop:
+  playTetris()
