03cea00f3552150f79a03a02a6c4750300f5ea15
TheCell <simon_hischier@hotmail.com>
2022-05-21 22:12:46 +0200

    used rng library

--- rust-learning/guessing_game/src/main.rs
+++ rust-learning/guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+// use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -12,4 +17,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    // match guess.cmp(&secret_number) {
+    //     Ordering::Less => println!("Too small!"),
+    //     Ordering::Greater => println!("Too big!"),
+    //     Ordering::Equal => println!("You win!"),
+    // }
 }
