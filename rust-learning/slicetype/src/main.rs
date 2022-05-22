13597a54f623286e0b3e8a3e88416516007ff7f7
TheCell <simon_hischier@hotmail.com>
2022-05-22 21:26:35 +0200

    slices

---
+++ rust-learning/slicetype/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear(); // error! if we use s later on like in println
+
+    println!("the first word is: {}", word);
+    s.clear();
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
