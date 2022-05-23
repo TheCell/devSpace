34a0d5f2e2bd8a2e40628a11cb8cf9961ad95d7b
TheCell <simon_hischier@hotmail.com>
2022-05-23 22:39:52 +0200

    added implementation block

--- rust-learning/rectangles/src/main.rs
+++ rust-learning/rectangles/src/main.rs
@@ -4,23 +4,54 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let sq = Rectangle::square(3);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        // area(&rect1)
+        rect1.area()
     );
     
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
     dbg!(&rect1);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&sq));
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
