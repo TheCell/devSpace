4501f55866371caceb13fd72cce03c1b847c955f
TheCell <simon_hischier@hotmail.com>
2022-07-30 23:55:21 +0200

    rust enum example

---
+++ rust-learning/enumDefinition/src/main.rs
@@ -0,0 +1,27 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("{}", home.address);
+    println!("{}", loopback.address);
+}
+
+
